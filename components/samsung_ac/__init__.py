f63d624ed087f5b5f743d8e54951c1972d641c34
Phil Morgan <phil@psmorgan.co.uk>
2025-05-22 10:49:18 +0100

    Fix deprecated default value
    The _UNDEF value was removed in https://github.com/esphome/esphome/pull/8725

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -143,12 +143,12 @@ CUSTOM_SENSOR_SCHEMA = sensor.sensor_schema().extend(
 
 def custom_sensor_schema(
     message: int,
-    unit_of_measurement: str = sensor._UNDEF,
-    icon: str = sensor._UNDEF,
-    accuracy_decimals: int = sensor._UNDEF,
-    device_class: str = sensor._UNDEF,
-    state_class: str = sensor._UNDEF,
-    entity_category: str = sensor._UNDEF,
+    unit_of_measurement: str = cv.UNDEFINED,
+    icon: str = cv.UNDEFINED,
+    accuracy_decimals: int = cv.UNDEFINED,
+    device_class: str = cv.UNDEFINED,
+    state_class: str = cv.UNDEFINED,
+    entity_category: str = cv.UNDEFINED,
     raw_filters=[],
 ):
     return sensor.sensor_schema(
